9dc70df74a12bfafd40c4061891eacff2bce0dfa
Yi Tseng <a86487817@gmail.com>
2016-11-11 11:27:52 +0800

    improve code readability by ES6(7)

--- autoupdate.js
+++ autoupdate.js
@@ -1,53 +1,48 @@
 #!/usr/bin/env node
 
-var _ = require('lodash');
-var fs = require('fs-extra');
-var async = require('async');
-var config = require('./config');
-var glob = require('glob');
-var GIT_REPO_LOCAL_FOLDER = config.GIT_REPO_LOCAL_FOLDER;
-var gitUpdater = require('./updaters/git');
+var fs = require('fs-extra')
+var async = require('async')
+var config = require('./config')
+var glob = require('glob')
+var GIT_REPO_LOCAL_FOLDER = config.GIT_REPO_LOCAL_FOLDER
+var gitUpdater = require('./updaters/git')
 
-var startAutoUpdate = function(library, callback) {
-  console.log('\n');
-  console.log(library.name.yellow);
-  var source = library.autoupdate.source;
+var startAutoUpdate = (library, callback) => {
+  console.log('\n')
+  console.log(library.name.yellow)
+  var source = library.autoupdate.source
   switch (source) {
     case 'git':
-      gitUpdater.update(library, callback);
-      break;
+      gitUpdater.update(library, callback)
+      break
     default:
-      console.log('Autoupdate type not supportted'.red);
-      callback(null, 0);
+      console.log('Autoupdate type not supportted'.red)
+      callback(null, 0)
   }
-};
+}
 
-var initialize = function(err) {
+var initialize = (err) => {
   if (err) {
-    console.error("Got an error: " + err);
+    console.error("Got an error: " + err)
   } else {
-    console.log('Starting Auto Update'.cyan);
-    console.log('-----------------------');
-    var args = process.argv.slice(2);
-    var globPattern = (args.length === 1) ? args[0] : '*';
-    var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/" + globPattern + "/package.json");
-    var librarys = _.chain(filenames)
-      .map(function(filename) {
-        return JSON.parse(fs.readFileSync(filename, 'utf8'));
-      })
-      .filter(function(library) {
-        return typeof library.autoupdate === 'object';
-      })
-      .value();
-    async.eachLimit(librarys, 8, function(library, callback) {
-      startAutoUpdate(library, callback);
-    }, function() {
-      console.log('\n');
-      console.log('-----------------------');
-      console.log('Auto Update Completed'.green);
-    });
-  }
-};
+    console.log('Starting Auto Update'.cyan)
+    console.log('-----------------------')
+    var args = process.argv.slice(2)
+    var globPattern = (args.length === 1) ? args[0] : '*'
+    var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/" + globPattern + "/package.json")
+    var librarys = filnames
+      .map((filename) => JSON.parse(fs.readFileSync(filename, 'utf8')))
+      .filter((library) => typeof library.autoupdate === 'object')
 
-fs.mkdirpSync(GIT_REPO_LOCAL_FOLDER);
-initialize();
+    async.eachLimit(librarys, 8, (library, callback) => {
+      startAutoUpdate(library, callback)
+    }, () => {
+      console.log('\n')
+      console.log('-----------------------')
+      console.log('Auto Update Completed'.green)
+    })
+  }
+}
+
+fs.mkdirpSync(GIT_REPO_LOCAL_FOLDER)
+initialize()
--- updaters/git.js
+++ updaters/git.js
@@ -1,154 +1,188 @@
+let config = require('../config')
+let GIT_REPO_LOCAL_FOLDER = config.GIT_REPO_LOCAL_FOLDER
+let git = require('gift')
+let asyncLib = require('asyncLib')
+let _ = require('lodash')
+let path = require('path')
+let glob = require('glob')
+let cdnjs = require('./cdnjs')
+let fs = require('fs-extra')
+let stable = require('semver-stable')
+let compareVersions = require('compare-versions')
+let colors = require('colors')
+let isThere = require('is-there')
 
-var config = require('../config');
-var GIT_REPO_LOCAL_FOLDER = config.GIT_REPO_LOCAL_FOLDER;
-var git = require('gift');
-var async = require('async');
-var _ = require('lodash');
-var path = require('path');
-var glob = require('glob');
-var cdnjs = require('./cdnjs');
-var fs = require('fs-extra');
-var stable = require('semver-stable');
-var compareVersions = require('compare-versions');
-var colors = require('colors');
-var isThere = require('is-there');
+let gitClone = (target, localTarget) => {
+  return new Promise((resolve,  reject) => {
+    git.clone(target, localTarget, (err, repo) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
-var update = function(library, callback) {
-  var target = library.autoupdate.target;
-  var localTarget = path.normalize(path.join(GIT_REPO_LOCAL_FOLDER, library.name));
-  async.series([
-    function(next) {
-      if (!isThere(localTarget)) {
-        console.log('Clone', target, 'to', localTarget)
-        git.clone(target, localTarget, function(err, repo) {
-          if (err) {
-            throw err;
-          } else {
-            next();
+let gitRemoteFetch = (repo, remoteName) => {
+  return new Promise((resolve, reject) => {
+    repo.remote_fetch(remoteName, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+let getTags = (repo) => {
+  return new Promise(resolve, reject) => {
+    repo.tags((err, tags) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(tags)
+      }
+    })
+  }
+}
+
+let gitCheckout = (repo, tag) => {
+  return new Promise((resolve, reject) => {
+    repo.checkout(tag, (err) => {
+      if(err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+let canBeUpdateed = (allFiles, library, greaterVer, tag) => {
+  return (allFiles.length !== 0) &&
+    (
+      (!library.version) ||
+      (
+        (greaterVer) &&
+        (
+          (stable.is(tag)) ||
+          (!stable.is(tag) && !stable.is(library.version))
+        )
+      )
+    )
+
+}
+
+let ensureFile = (file, fileTarget) => {
+  return new Promise((resolve, reject) => {
+    fs.ensureFile(fileTarget, async (err) => {
+      if (err) {
+        console.log('Some strange error occured here'.red)
+        console.dir(err)
+        reject(err)
+      } else {
+        await copyFile(file._, fileTarget)
+        resolve()
+      }
+    })
+  })
+}
+
+let copyFile = (fileSource, fileTarget) => {
+  return new Promise((resolve, reject) => {
+    fs.copy(fileSource, fileTarget, (err) => {
+      if (err) {
+        console.dir(err)
+        console.log('Some strange error occured here'.red)
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+let update = (library, callback) => {
+  var target = library.autoupdate.target
+  var localTarget = path.normalize(path.join(GIT_REPO_LOCAL_FOLDER, library.name))
+
+  try {
+    await gitClone(target, localTarget)
+    var repo = git(localTarget)
+    console.log('Use', localTarget, 'as source of', library.name)
+    await gitRemoteFetch(repo, 'origin')
+    let tags = await getTags(repo)
+    let versions = tags.map((tag) => tag.name)
+    let needed = versions.filter((version) => {
+      if (version.toLowerCase().startsWith('v')) {
+        version = version.substr(1)
+      }
+      return (!cdnjs.checkVersion(library, version) && /\d+/.test(version))
+    })
+
+    if (needed.length > 0) {
+      console.log(library.name, 'needs versions:', needed.join(',').blue)
+    }
+
+    needed.forEach((tag) => {
+      await gitCheckout(repo, tag)
+      if (tag.toLowerCase.startsWith('v')) {
+        tag = tag.substr(1)
+      }
+      var basePath = library.autoupdate.basePath || ""
+      var libContentsPath = path.normalize(path.join(localTarget, basePath))
+      var allFiles = []
+
+      library.autoupdate.fileMap.forEach((mapGroup) => {
+        let cBasePath = mapGroup.basePath || "", files = []
+        libContentsPath = path.normalize(path.join(localTarget, cBasePath)),
+        mapGroup.files.forEach((cRule) => {
+          let newFiles = glob.sync(path.normalize(path.join(libContentsPath, cRule)), {nodir: true, realpath: true})
+          files = files.concat(newFiles)
+          if (newFiles.length === 0) {
+            console.log('Not found'.red, cRule.cyan, tag)
+            fs.mkdirsSync(path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, tag)))
           }
         })
-      } else {
-        next();
-      }
-    },
-    function(next) {
-      var repo = git(localTarget);
-      console.log('Use', localTarget, 'as source of', library.name);
-      repo.remote_fetch('origin', function(err) {
-        if (err) {
-          console.dir(err);
-          console.log(library.name, 'git repo fetch failed');
-          next();
-        }
-        repo.tags(function(err, tags) {
-          if (err) {
-            console.log(library.name, 'git tag handle failed');
-            console.dir(err);
-            next();
-          }
-          var versions = _.map(tags, function(tag) {
-            return tag.name;
-          });
-          var needed = _.filter(versions, function(version) {
-            if (version[0] === 'v' || version[0] === 'V') {
-              version = version.substr(1);
-            }
-            return (!cdnjs.checkVersion(library, version) && /\d+/.test(version));
-          });
-          if (needed.length > 0) {
-            console.log(library.name, 'needs versions:', needed.join(',').blue);
-          }
-          async.eachSeries(needed, function(tag, callback) {
-            repo.checkout(tag, function() {
-              if (tag[0] === 'v' || tag[0] === 'V') {
-                tag = tag.substr(1);
-              }
-              var basePath = library.autoupdate.basePath || "";
-              var libContentsPath = path.normalize(path.join(localTarget, basePath));
-              var allFiles = [];
 
-              _.each(library.autoupdate.fileMap, function(mapGroup) {
-                 var cBasePath = mapGroup.basePath || "",
-                     files = [];
-                 libContentsPath = path.normalize(path.join(localTarget, cBasePath)),
-                 _.each(mapGroup.files, function (cRule) {
-                     var newFiles = glob.sync(path.normalize(path.join(libContentsPath, cRule)), {nodir: true, realpath: true});
-                     files = files.concat(newFiles);
-                     if (newFiles.length === 0) {
-                      console.log('Not found'.red, cRule.cyan, tag);
-                      fs.mkdirsSync(path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, tag)));
-                     }
-                 });
-                 allFiles = allFiles.concat(files.map(function (c) {
-                     return {
-                         _: c,
-                         basePath: cBasePath
-                     }
-                 }));
-              });
-              console.log('All files for ' + library.name + ' v' + tag, '-', allFiles.length);
-              console.log(allFiles.length, allFiles.length !== 0);
-              library.version = library.version || "0.0.0";
-              var greaterVer;
-              try {
-                greaterVer = compareVersions(tag, library.version) > 0;
-              } catch (e) {
-                greaterVer = false;
-              }
-              if (
-                (allFiles.length !== 0) &&
-                (
-                  (!library.version) ||
-                  (
-                    (greaterVer) &&
-                    (
-                      (stable.is(tag)) ||
-                      (!stable.is(tag) && !stable.is(library.version))
-                    )
-                  )
-                )
-              ) {
-                console.log('Updated package.json to version'.green, tag);
-                var libraryPath = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, 'package.json'));
-                var libraryJSON = JSON.parse(fs.readFileSync(libraryPath, 'utf8'));
-                libraryJSON.version = tag;
-                fs.writeFileSync(libraryPath, JSON.stringify(libraryJSON, undefined, 2) + '\n');
-              }
-              async.each(allFiles, function(file, callback) {
-                var fileName = path.relative(path.join(localTarget, file.basePath), file._);
-                var fileTarget = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, tag, fileName));
-                fs.ensureFile(fileTarget, function(err) {
-                  if (err) {
-                    console.log('Some strange error occured here'.red);
-                    console.dir(err);
-                    callback();
-                  } else {
-                    fs.copy(file._, fileTarget, function(err) {
-                      if (err) {
-                        console.dir(err);
-                        console.log('Some strange error occured here'.red);
-                        callback();
-                      } else {
-                        callback();
-                      }
-                    });
-                  }
-                });
-              }, function() {
-                callback();
-              });
-            });
-          }, function() {
-            console.log(library.name.green, 'updated from Git'.green);
-            callback(null, 1);
-          });
-        });
-      });
-    }
-  ]);
-};
+        allFiles = allFiles.concat(files.map((c) => ({_: c,basePath: cBasePath})))
+      })
+
+      console.log('All files for ' + library.name + ' v' + tag, '-', allFiles.length)
+      console.log(allFiles.length, allFiles.length !== 0)
+
+      library.version = library.version || "0.0.0"
+      var greaterVer
+      try {
+        greaterVer = compareVersions(tag, library.version) > 0
+      } catch (e) {
+        greaterVer = false
+      }
+
+      if (canBeUpdateed(allFiles, library, greaterVer, tag)) {
+        console.log('Updated package.json to version'.green, tag)
+        var libraryPath = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, 'package.json'))
+        var libraryJSON = JSON.parse(fs.readFileSync(libraryPath, 'utf8'))
+        libraryJSON.version = tag
+        fs.writeFileSync(libraryPath, JSON.stringify(libraryJSON, undefined, 2) + '\n')
+      }
+
+      allFiles.forEach((file) => {
+        var fileName = path.relative(path.join(localTarget, file.basePath), file._)
+        var fileTarget = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', library.name, tag, fileName))
+        await ensureFile(fileTarget)
+      })
+    })
+
+    console.log(library.name.green, 'updated from Git'.green)
+
+  } catch (err) {
+    git.dir(err)
+  }
+}
 
 module.exports = {
   update: update
-};
-
+}
